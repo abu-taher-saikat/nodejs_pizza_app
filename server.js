a7f47492e78250e49c540fd67a57b9a4894672c6
abutaher <mdabutahersaikat@gmail.com>
2021-01-07 22:56:08 +0600

    session added to webpage

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const path = require('path')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash'); //for session perpurs
+const MongoDbStore = require('connect-mongo')(session) //session store.
+
+
+// Database connection.
+const url = "mongodb+srv://saikat:saikat1095@cluster0.htwdq.mongodb.net/pizza_app?retryWrites=true&w=majority";
+mongoose.connect(url, {useNewUrlParser : true, useCreateIndex : true, useUnifiedTopology: true, useFindAndModify : true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database connected...');
+}).catch(err=> {
+    console.log('Connection failed...');
+})
+
+// Session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection : 'sessions'
+})
+
+// Session config
+app.use(session({
+    secret : process.env.COOKIE_SECRET,
+    resave : false,
+    store : mongoStore,
+    saveUninitialized : false,
+    cookie : {maxAge : 1000 * 60 * 24 } //24 Hours
+    // cookie : {maxAge : 1000 * 15 } //15sec
+}));
+
+
+app.use(flash());
 
 // Assets 
 app.use(express.static('public'))
@@ -11,24 +46,7 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs')
 
-
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-app.get('/cart', (req, res)=> {
-    res.render('customerse/cart')
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-});
-
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-});
-
-
+require('./routes/web')(app);
 
 
 const PORT = process.env.PORT ||  3300;
