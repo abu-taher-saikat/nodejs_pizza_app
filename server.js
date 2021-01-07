5a86ed34a4bd71f27e869634606e9f09530e23b7
abutaher <mdabutahersaikat@gmail.com>
2021-01-07 10:46:20 +0600

    done

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 
+// Assets 
+app.use(express.static('public'))
+// Set Template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs')
 
 
 app.get('/',(req,res)=>{
@@ -11,17 +17,18 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/cart', (req, res)=> {
-    re
+    res.render('customerse/cart')
 })
 
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+});
 
 
-// Assets 
-app.use(express.static('public'))
-// Set Template engine
-app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs')
 
 
 const PORT = process.env.PORT ||  3300;
