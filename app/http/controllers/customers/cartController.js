dc539ccddc79b6696e97396326d581fdda81d3d7
abutaher <mdabutahersaikat@gmail.com>
2021-01-08 23:42:26 +0600

    cart js coding structure added here.

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -1,9 +1,44 @@
-function cartController(){
+function cartController() {
     return {
-        index(req,res){
+        index(req, res) {
             res.render('customerse/cart')
+        },
+
+        update(req, res) {
+            // For the first time creating cart and adding basic object structure . 
+            if (!req.session.cart) {
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0
+                }
+            }
+            
+            let cart = req.session.cart;
+
+            // Check if item does not exist in cart. 
+            if (!cart.items[req.body._id]) {
+                cart.items[req.body._id] = {
+                    items: req.body,
+                    qty: 1
+                }
+
+                cart.totalQty = cart.totalQty + 1;
+                cart.totalPrice = cart.totalPrice + req.body.price;
+            } else {
+                cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
+                cart.totalQty = cart.totalQty + 1;
+                cart.totalPrice = cart.totalPrice + req.body.price
+            }
+
+            // let cart = req.session.cart;
+            // console.log(req.body);
+            return res.json({
+                totalQty: req.session.cart.totalQty
+            })
+
         }
-    }   
+    }
 }
 
 module.exports = cartController;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,23 @@
-console.log('hello from node js')
+import axios from 'axios';
 
-var a = "saikat"
-console.log(a);
+let addToCart = document.querySelectorAll('.add-to-cart');
+let cartCounter = document.querySelector('#cartCounter');
+
+// update cart
+function updateCart(pizza){
+    // 
+    axios.post('/update-cart', pizza).then(res => {
+        console.log(res);
+        cartCounter.innerText = res.data.totalQty;
+    })
+}
+
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        //let pizza = btn.dataset.pizza; //dataset => work for getting data-pizza attribute form home.ejs.(String)
+        let pizza = JSON.parse(btn.dataset.pizza); //dataset => work for getting data-pizza attribute form home.ejs.(object)
+        updateCart(pizza);
+        // console.log(pizza);
+    })
+})
